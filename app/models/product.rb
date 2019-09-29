0caabd8d1492f34b74b26cffa8cb9e772e9ac555
da <dm.ilin.aleksandrovich@gmail.com>
2019-09-29 19:59:39 +0300

    Some fix for more readble

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,7 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates_inclusion_of :domain, in: DOMAINS, allow_nil: true
   validates_inclusion_of :device, in: DEVICES, allow_nil: true
-  validates_inclusion_of :os,     in: OS, allow_nil: true
+  validates_inclusion_of :os,     in: OS,      allow_nil: true
 
   accepts_nested_attributes_for :blocks, reject_if: :all_blank, allow_destroy: true
 end
